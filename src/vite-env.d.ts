e1113aafd0914c9d4e4434daedf343db593b3c10
tamighi <thomas.amighi@hotmail.com>
2025-07-21 17:44:53 +0200

    chore: glsl plugin

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,33 +4,3 @@ declare module "*.glsl" {
   const src: string;
   export default src;
 }
-
-declare module "*.frag" {
-  const src: string;
-  export default src;
-}
-
-declare module "*.vert" {
-  const src: string;
-  export default src;
-}
-
-declare module "*.gltf" {
-  const src: string;
-  export default src;
-}
-
-declare module "*.glb" {
-  const src: string;
-  export default src;
-}
-
-declare module "*.exr" {
-  const src: string;
-  export default src;
-}
-
-declare module "*.hdr" {
-  const src: string;
-  export default src;
-}
