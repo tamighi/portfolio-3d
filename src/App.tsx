aeeb54c72d4c61948de835831468345a8056c118
Amighi Thomas <thomas.amighi@hotmail.com>
2025-09-03 09:45:27 +0200

    feat: in progress component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Canvas } from "@react-three/fiber";
 import "./index.css";
 import Scene from "./scene/Scene";
+import InProgress from "./components/InProgress";
 
 const App = () => {
   return (
     <div className="w-screen h-screen">
+      <InProgress />
       <Canvas>
         <Scene />
       </Canvas>
---
+++ src/components/InProgress.tsx
@@ -0,0 +1,9 @@
+const InProgress = () => {
+  return (
+    <div className="fixed top-0 right-0 left-0 bg-red-500 flex justify-center z-50 p-2">
+      <span>Portfolio in progress ...</span>
+    </div>
+  );
+};
+
+export default InProgress;
